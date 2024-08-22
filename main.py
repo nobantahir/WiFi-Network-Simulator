d0e9352e29a8e750ba9a6437d257802b5b0340a0
Noban Tahir <mntahir@uci.edu>
2024-08-21 21:38:19 -0700

    Updating last state.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import math
 
 
-test_power = 100
-test_frequency = 2.4
-test_distance = 1
+test_power = 50
+test_frequency = 5000
+test_distance = 10
 
 print("Distance loss: ", 20 * math.log10(test_distance))
 print("Frequency loss: ", 20 * math.log10(test_frequency))
@@ -12,3 +12,21 @@ print("Frequency loss: ", 20 * math.log10(test_frequency))
 calc_rssi = test_power - (20 * math.log10(test_distance)) - (20 * math.log10(test_frequency)) - 32.44
 
 print(calc_rssi)
+
+def acceptable_input(zero):
+    assert zero in ["AP", "CLIENT", "MOVE"], "Error, input does not match expected."
+def parse_line(line):
+    data = line.strip("\n").split(' ')
+    acceptable_input(data[0])
+    print(data, " length: ", len(data))
+    
+
+
+# Main is the Access Controller
+
+print("Enter Simulation File")
+#sim = input()
+sim = "C:\\Users\\noban\\Desktop\\P3\\sim.txt"
+with open(sim, 'r') as sim_file:
+    for line in sim_file:
+        parse_line(line)
