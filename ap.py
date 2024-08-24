24c4c748e24c74da3b388e8556cb555a32d8d972
Noban Tahir <mntahir@uci.edu>
2024-08-24 16:30:21 -0700

    Corrected type of frequency to string.

--- ap.py
+++ ap.py
@@ -1,7 +1,7 @@
 from network import Network
 
 class AccessPoint(Network):
-    def __init__(self, name: str, x: int, y: int, channel: int, power_level: int, frequency: int, standard: str, support11k: bool, support11v: bool, support11r: bool, coverage_radius: int, device_limit: int, min_rssi = None):
+    def __init__(self, name: str, x: int, y: int, channel: int, power_level: int, frequency: str, standard: str, support11k: bool, support11v: bool, support11r: bool, coverage_radius: int, device_limit: int, min_rssi = None):
         super().__init__(x, y, support11k, support11v, support11r, min_rssi)
         self.name = name
         self.channel = channel
