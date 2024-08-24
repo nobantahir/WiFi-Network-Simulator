544dfbfefd6965e20659f4f8e2335c4e5c5c0574
Noban Tahir <mntahir@uci.edu>
2024-08-24 16:25:39 -0700

    Correcrted AP to include a name for the access point.

--- ap.py
+++ ap.py
@@ -1,8 +1,9 @@
 from network import Network
 
 class AccessPoint(Network):
-    def __init__(self, x: int, y: int, channel: int, power_level: int, frequency: int, standard: str, support11k: bool, support11v: bool, support11r: bool, coverage_radius: int, device_limit: int, min_rssi = None):
+    def __init__(self, name: str, x: int, y: int, channel: int, power_level: int, frequency: int, standard: str, support11k: bool, support11v: bool, support11r: bool, coverage_radius: int, device_limit: int, min_rssi = None):
         super().__init__(x, y, support11k, support11v, support11r, min_rssi)
+        self.name = name
         self.channel = channel
         self.power_level = power_level
         self.frequency = frequency
--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 from network import Network
 
 class Client(Network):
-    def __init__(self, x: int, y: int, support11k: bool, support11v: bool, support11r: bool, min_rssi = -60):
+    def __init__(self, x: int, y: int, support11k: bool, support11v: bool, support11r: bool, min_rssi: int):
         super().__init__(x, y, support11k, support11v, support11r, min_rssi)
+        
         
