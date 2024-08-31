607280dd3efa4135d148538b9545a153a6ba130d
Noban Tahir <mntahir@uci.edu>
2024-08-31 15:37:26 -0700

    Added logic to ignore blank lines.

--- main.py
+++ main.py
@@ -9,25 +9,29 @@ def acceptable_input(zero):
 
 
 def parse_line(line):
-    data = line.strip("\n").split(' ')
-    line_call = acceptable_input(data[0])
-    if line_call == "AP":
-        if len(data) == 14:
-            ap = AccessPoint(data[1], int(data[2]), int(data[3]), int(data[4]), int(data[5]), data[6], data[7], (data[8]), (data[9]), (data[10]), int(data[11]), int(data[12]), int(data[13]))
-            access_points.append(ap)
-        elif len(data) == 13:
-            ap = AccessPoint(data[1], int(data[2]), int(data[3]), int(data[4]), int(data[5]), data[6], data[7], (data[8]), (data[9]), (data[10]), int(data[11]), int(data[12]))
-            access_points.append(ap)
-    elif line_call == "CLIENT":
-        if len(data) == 10:
-            client = Client(data[1], int(data[2]), int(data[3]), data[4], data[5], (data[6]), (data[7]), (data[8]), int(data[9]))
-            clients.append(client)
-            simulation.append(client)
-    elif line_call == "MOVE":
-        if len(data) == 4:
-            move = (data[1], int(data[2]), int(data[3]))
-            moves.append(move)
-            simulation.append(move)
+    # If the line is blank we don't have to parse it.
+    if line.isspace():
+        pass
+    else:
+        data = line.strip("\n").split(' ')
+        line_call = acceptable_input(data[0])
+        if line_call == "AP":
+            if len(data) == 14:
+                ap = AccessPoint(data[1], int(data[2]), int(data[3]), int(data[4]), int(data[5]), data[6], data[7], (data[8]), (data[9]), (data[10]), int(data[11]), int(data[12]), int(data[13]))
+                access_points.append(ap)
+            elif len(data) == 13:
+                ap = AccessPoint(data[1], int(data[2]), int(data[3]), int(data[4]), int(data[5]), data[6], data[7], (data[8]), (data[9]), (data[10]), int(data[11]), int(data[12]))
+                access_points.append(ap)
+        elif line_call == "CLIENT":
+            if len(data) == 10:
+                client = Client(data[1], int(data[2]), int(data[3]), data[4], data[5], (data[6]), (data[7]), (data[8]), int(data[9]))
+                clients.append(client)
+                simulation.append(client)
+        elif line_call == "MOVE":
+            if len(data) == 4:
+                move = (data[1], int(data[2]), int(data[3]))
+                moves.append(move)
+                simulation.append(move)
 
 # use if variable is False for min rssi  
 
